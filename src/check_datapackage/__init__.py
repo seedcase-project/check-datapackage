c4e144b9fe67d007d768c9b8a3dbec86805ccb6a
martonvago <marci.vago@gmail.com>
2025-09-18 10:24:12 +0100

    feat: :sparkles: expose classes in init

--- src/check_datapackage/__init__.py
+++ src/check_datapackage/__init__.py
@@ -5,15 +5,21 @@ from .check_error_matcher import CheckErrorMatcher
 from .check_package_properties import check_package_properties
 from .check_properties import check_properties
 from .check_resource_properties import check_resource_properties
+from .config import Config
 from .constants import (
     PACKAGE_RECOMMENDED_FIELDS,
     PACKAGE_REQUIRED_FIELDS,
     RESOURCE_REQUIRED_FIELDS,
     RequiredFieldType,
 )
+from .exclude import Exclude
 from .exclude_matching_errors import exclude_matching_errors
+from .rule import Rule
 
 __all__ = [
+    "Config",
+    "Exclude",
+    "Rule",
     "CheckError",
     "CheckErrorMatcher",
     "check_properties",
