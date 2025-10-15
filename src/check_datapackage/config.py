c8e1f8a707383669dce9a2c9d34e96a3af866bb4
Signe Kirk Brødbæk <signebroedbaek@gmail.com>
2025-10-15 11:13:26 +0200

    refactor: 🚚 `Rule` to `CustomCheck` (#133)
    # Description
    
    To keep things simple(r), this only renames and rephrases files,
    classes, functions, and text related to `Rule` to `CustomCheck`.
    Renaming of anything with `Exclude` will be done in a separate PR.
    
    One part of #103  
    
    Needs an in-depth review.
    
    ## Checklist
    
    - [X] Formatted Markdown
    - [X] Ran `just run-all`

--- src/check_datapackage/config.py
+++ src/check_datapackage/config.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass, field
 from typing import Literal
 
+from check_datapackage.custom_check import CustomCheck
 from check_datapackage.exclude import Exclude
-from check_datapackage.rule import Rule
 
 
 @dataclass
@@ -12,8 +12,8 @@ class Config:
     Attributes:
         exclude (list[Exclude]): Any issues matching any of these exclusions will be
             ignored (i.e., removed from the output of the check function).
-        rules (list[Rule]): Custom checks listed here will be done in addition
-            to checks defined in the Data Package standard.
+        custom_checks (list[CustomCheck]): Custom checks listed here will be done in
+            addition to checks defined in the Data Package standard.
         strict (bool): Whether to run recommended as well as required checks. If
             True, recommended checks will also be run. Defaults to False.
         version (str): The version of the Data Package standard to check against.
@@ -24,17 +24,17 @@ class Config:
         import check_datapackage as cdp
 
         exclude_required = cdp.Exclude(type="required")
-        license_rule = cdp.Rule(
+        license_check = cdp.CustomCheck(
             type="only-mit",
             jsonpath="$.licenses[*].name",
             message="Data Packages may only be licensed under MIT.",
             check=lambda license_name: license_name == "mit",
         )
-        config = cdp.Config(exclude=[exclude_required], rules=[license_rule])
+        config = cdp.Config(exclude=[exclude_required], custom_checks=[license_check])
         ```
     """
 
     exclude: list[Exclude] = field(default_factory=list)
-    rules: list[Rule] = field(default_factory=list)
+    custom_checks: list[CustomCheck] = field(default_factory=list)
     strict: bool = False
     version: Literal["v1", "v2"] = "v2"
