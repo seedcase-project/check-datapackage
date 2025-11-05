c89dca7d71ed98c7d46e8b3e7295ddb6b803a26c
Marton Vago <marci.vago@gmail.com>
2025-11-05 17:08:16 +0000

    chore: :wrench: add to vulture-allowlist

--- tools/vulture-allowlist.py
+++ tools/vulture-allowlist.py
@@ -1,3 +1,6 @@
 # ruff: noqa
 # mypy: ignore-errors
 version  # unused variable (src/check_datapackage/config.py:44)
+_check_not_required  # unused method (src/check_datapackage/extensions.py:59)
+_check_field_name_in_jsonpath  # unused method (src/check_datapackage/extensions.py:117)
+cls  # unused variable (src/check_datapackage/extensions.py:61)
